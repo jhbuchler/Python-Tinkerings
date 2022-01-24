2ef56175dcd46939e02dd22d3846ae6b1217e138
jhbuchler <86932152+jhbuchler@users.noreply.github.com>
2022-01-24 15:13:07 -0500

    Update phoneregex.py

--- phoneregex.py
+++ phoneregex.py
@@ -6,7 +6,7 @@ phoneRegex = re.compile(r'''
 (
 ((\d\d\d) | (\(d\d\d\)))?    # area code (optional)
 (\s|-)                       # first separator
-\d\d\d                       # first 33 digits
+\d\d\d                       # first 3 digits
 -                            # separator
 \d\d\d\d                     # last 4 digits
 (((ext(\.)?\s) |x)           # extension word-part (optional)
